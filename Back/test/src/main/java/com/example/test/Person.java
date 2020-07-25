53c31f1200d786a5775b7205adfe9d2264a565d0
daceroso <daceros19@gmail.com>
2020-07-24 19:07:58 -0500

    Add files via upload
    update folder back

--- Back/test/src/main/java/com/example/test/Person.java
+++ Back/test/src/main/java/com/example/test/Person.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.persistence.*;
 import java.io.Serializable;
 
+
 @Data
 @Entity
 @Table(name = "person")
@@ -12,14 +13,18 @@ public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idPerson;
+
+   // @NotEmpty
     @Column(name= "first_name")
     private String firstName;
+    //@NotEmpty
     @Column(name = "last_name")
     private String lastName;
     @Column(name = "email")
     private String email;
+   // @NotEmpty
     @Column(name = "gender")
     private String gender;
 
---
+++ Back/test/src/main/java/com/example/test/services/PersonServiceImplemetation.java
@@ -0,0 +1,40 @@
+package com.example.test.services;
+
+import com.example.test.dao.PersonDao;
+import com.example.test.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class PersonServiceImplemetation implements PersonService{
+
+    @Autowired
+    private PersonDao personDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Person> listPeople() {
+        return (List<Person>) personDao.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void savePerson(Person person) {
+         personDao.save(person);
+    }
+
+    @Override
+    @Transactional
+    public void deletePerson(Person person) {
+        personDao.delete(person);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Person findPerson(Person person) {
+        return personDao.findById(person.getIdPerson()).orElse(null);
+    }
+}
